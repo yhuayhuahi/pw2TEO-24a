ca07273007b5fe0b1da53f85dbab4c6e9a54c389
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-06-15 20:42:21 -0500

    Se usa variables de contexto en la plantilla {{ myvariable }}

--- django-practice3/personas/views.py
+++ django-practice3/personas/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(request):
-    return render(request, 'home.html')
+def myHomeView(request, *args, **kargs):
+    mycontext = {
+        'mytext': 'Esto es sobre nosotros :|',
+        'mynumber': 123
+    }
+    return render(request, 'home.html', mycontext)
