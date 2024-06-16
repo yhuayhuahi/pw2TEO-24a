a9171753de3b9f6ad92eed404daf59c1d20cd85d
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-06-15 20:49:46 -0500

    Pruebo el tag for en home.html

--- django-practice3/personas/views.py
+++ django-practice3/personas/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 def myHomeView(request, *args, **kargs):
     mycontext = {
         'mytext': 'Esto es sobre nosotros :|',
-        'mynumber': 123
+        'mynumber': 123,
+        'mylist': [1, 2, 3, 4, 5, 6, 7]
     }
     return render(request, 'home.html', mycontext)
