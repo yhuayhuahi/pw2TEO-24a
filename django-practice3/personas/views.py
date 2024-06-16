02b264828abfeea9547ecd59cb4e6fc91b2d6d28
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-06-15 21:01:12 -0500

    Probando if en home.html

--- django-practice3/personas/views.py
+++ django-practice3/personas/views.py
@@ -6,6 +6,6 @@ def myHomeView(request, *args, **kargs):
     mycontext = {
         'mytext': 'Esto es sobre nosotros :|',
         'mynumber': 123,
-        'mylist': [1, 2, 3, 4, 5, 6, 7]
+        'mylist': [33, 44, 55]
     }
     return render(request, 'home.html', mycontext)
