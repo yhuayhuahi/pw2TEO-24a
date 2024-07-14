98221fc9d6d5ac185601aeceae6f977e18517a17
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-07-13 19:39:06 -0500

    En este punto estoy en la diapositiva 7, se creo el la tabla movie en models.py

--- djangular/djangular/api/models.py
+++ djangular/djangular/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length = 32)
+    desc = models.CharField(max_length = 256)
+    year = models.IntegerField()
+# No olvidar hacer:   $ python manage.py makemigrations | python manage.py migrate
