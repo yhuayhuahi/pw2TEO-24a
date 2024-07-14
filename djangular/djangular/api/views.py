65f393b919054ba8975f20ec13b2bca8134290f1
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2024-07-13 19:43:39 -0500

    En este punto me encuentro en la diapositiva 10, en donde me aparece la interdfaz de Django Rest Framework

--- djangular/djangular/api/views.py
+++ djangular/djangular/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from .serializer import MovieSerializer
+from .models import Movie
 
-# Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
